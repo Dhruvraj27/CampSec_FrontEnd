e941962f15b0b84adfc10d588d9fd1c837361b6f
Anand Tiwari <anandt607@gmail.com>
2024-04-18 21:40:13 +0530

    changes in the time and the date of the exit and entry time

--- src/Utils/GetTimeAndHour.js
+++ src/Utils/GetTimeAndHour.js
@@ -2,7 +2,7 @@ export const getEntryTime = () => {
   let year = new Date().getFullYear();
   let day = new Date().getDate();
   let month = new Date().getMonth();
-  let hours = new Date().getHours();
+  let hours = new Date().getHours() % 12 || 12;
   let Minutes = new Date().getMinutes();
   let seconds = new Date().getSeconds();
   let getDate = `${day}-${month}-${year}`;
