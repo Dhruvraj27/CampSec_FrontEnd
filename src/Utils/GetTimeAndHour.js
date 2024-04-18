76e190dbd257b249b543bffdc61af8b3b5c4dd66
Dhruvraj27 <73035080+Dhruvraj27@users.noreply.github.com>
2024-04-18 22:10:50 +0530

    adhkashdkhasd

--- src/Utils/GetTimeAndHour.js
+++ src/Utils/GetTimeAndHour.js
@@ -2,12 +2,12 @@ export const getEntryTime = () => {
   let year = new Date().getFullYear();
   let day = new Date().getDate();
   let month = new Date().getMonth();
+  let actualHours = new Date().getHours()
   let hours = new Date().getHours() % 12 || 12;
   let Minutes = new Date().getMinutes();
   let seconds = new Date().getSeconds();
   let getDate = `${day}-${month}-${year}`;
-  let getCurrentTime = `${hours}:${Minutes}:${seconds} ${
-    hours < 12 ? "AM" : "PM"
-  }`;
+  let getCurrentTime = `${hours}:${Minutes}:${seconds} ${actualHours < 12 ? "AM" : "PM"
+    }`;
   return `${getDate} ${getCurrentTime}`;
 };
